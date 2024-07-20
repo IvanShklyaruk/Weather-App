962731a78967c4efd19ccab09a1a4b70d6f6788a
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-19 22:07:23 -0400

    initial code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherAppPage from "./pages/WeatherAppPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherAppPage />
+    </div>
+  );
+}
+
+export default App;
