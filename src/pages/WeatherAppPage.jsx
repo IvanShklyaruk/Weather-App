2cb9c5f2a3c0be6a6ef1572dcd94951ccb48d419
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-20 00:40:05 -0400

    Weather app finished

---
+++ src/pages/WeatherAppPage.jsx
@@ -0,0 +1,12 @@
+import Weather from "../components/Weather";
+import "../styles/WeatherAppPage.css";
+
+const WeatherAppPage = () => {
+  return (
+    <div>
+      <Weather />
+    </div>
+  );
+};
+
+export default WeatherAppPage;
